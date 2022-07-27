b1b3cc543662b72754dc9d8ece10684292211ebb
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-27 13:23:54 +0200

    ok

---
+++ Password.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace T10_Ejerciocio3
+{
+    class Password
+    {
+        private int longitud;
+
+
+        public int Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+
+        private string contraseña;
+
+        public string getContraseña()
+        {
+            return contraseña;
+        }
+
+
+        public Password()
+        {
+            longitud = 8;
+            contraseña = generaPass(longitud);
+
+        }
+
+        public Password(int _longitud)
+        {
+            this.longitud = _longitud;
+            this.contraseña = generaPass(_longitud);
+        }
+
+        public string generaPass(int _longitud)
+        {
+            string charsPass = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+            char[] pass = new char[_longitud];
+            for (int i = 0; i < _longitud; i++)
+            {
+                pass[i] = charsPass[random.Next(0, charsPass.Length)];
+            }
+            //Convierto en el return el array en string.
+            return new string(pass); //Sería como decir string x = new string(pass)
+        }
+
+        public bool esFuerte(string _contraseña)
+        {
+            //Convierto en un array Char la contraseña en string
+            char[] passAChar = _contraseña.ToCharArray();
+            int counter = 0, counter1 = 0, counter2 = 0;
+            bool esFuerte = false;
+            for (int i = 0; i < passAChar.Length; i++)
+            {
+
+                if (char.IsUpper(passAChar[i]))
+                {
+                    counter++;
+                }
+                if (char.IsLower(passAChar[i]))
+                {
+                    counter1++;
+                }
+                if (char.IsDigit(passAChar[i]))
+                {
+                    counter2++;
+                }
+            }
+            if (counter > 2 && counter1 > 1 && counter2 > 5)
+            {
+                esFuerte = true;
+            }
+            return esFuerte;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T10_Ejerciocio3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el tamaño de los passwords (mínimo recomendanle 8): ");
+            int _longitud2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba cuántos passwords desea generar: ");
+            int _numPasswords = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            //Declaro un array de objetos
+            Password[] passwords = new Password[_numPasswords];
+            //Declaro array de booleanos relacionados con la fortaleza de la contraseña
+            bool[] passEsFuerte = new bool[_numPasswords];
+
+            for (int i = 0; i < _numPasswords; i++)
+            {
+                //Primero inicializamos cada objeto del array de objetos y usando el segundo constructor
+                passwords[i] = new Password(_longitud2);
+                //Generamos el password
+                Console.Write(passwords[i].generaPass(_longitud2)+" ");
+                //Analizo el password a la vez que analizo si es fuerte.
+                Console.WriteLine(passEsFuerte[i] = passwords[i].esFuerte(passwords[i].getContraseña()));                
+            }
+        }
+    }
+}
